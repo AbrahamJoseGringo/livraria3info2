18443e62de731f1cb1f754bec7ec8f534b11460b
Nome do usuário <aperez.castro32@gmail.com>
2024-08-26 10:39:41 -0300

    Livraria

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,11 +11,16 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet, LivroViewSet
+
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
-
-router.register(r"usuarios", UserViewSet, basename="usuarios")
+router.register(r"autores", AutorViewSet, basename="autores")
+router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"editoras", EditoraViewSet, basename="editoras")
+router.register(r"livros", LivroViewSet, basename="livros")
+router.register(r"usuarios", UserViewSet, basename="users")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,6 +39,11 @@ urlpatterns = [
     # Simple JWT
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Uploader
+    path("api/media/", include(uploader_router.urls)),
     # API
     path("api/", include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
+
